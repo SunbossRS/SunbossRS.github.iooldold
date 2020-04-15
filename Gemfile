07ca49cbdedd3ba18d0368a4ebbadbc8ea4fa084
SunbossRS <312723014@qq.com>
2020-04-16 01:34:38 +0800

    2020-04-15

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# source "https://rubygems.org"
+source 'https://gems.ruby-china.com'
+
+gem 'jekyll-paginate', '~> 1.0'
+gem 'jekyll-feed'
+gem "jekyll"
+gem "bundler"
+
---
+++ GitUpload_DateTime.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# ATTENTION! PUT THIS FILE 2 *YOUR PROJECT*/$gitflow
+# Visit https://github.com/sunbossrs/gitflow for more infos.
+
+DATE=$(date -d'1 day ago' "+%Y-%m-%d") && TIME=$(date "+%H:%M:%S") 
+echo -e "  ____\n |\n   The upload-to-git-platform script as date-time format commit\n                                                             ____| \n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n[+] Commiting changes ..." && git add . && git commit -a -m "$DATE $TIME" && echo "[+] Pushing ..." && git push
